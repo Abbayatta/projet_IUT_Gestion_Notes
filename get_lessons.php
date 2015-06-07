943b488fe477af8160428c6dac25bcf4b34fc059
Maxime DUFOUR <maxime-dufour@outlook.fr>
2015-06-07 23:57:45 +0200

    Finalisation de l'interface professeur
    Interface professeur me semble terminée !
    Enfin... Il manque la saisie des notes

--- get_lessons.php
+++ get_lessons.php
@@ -23,6 +23,23 @@
 		}
 	}
 	
+	function get_prof_lessons($id) {
+		include("pdo.php");
+		include("get_users.php");
+		include("get_groups.php");
+		$req=$pdo->query('SELECT * FROM lessons WHERE user_id='.$id);
+
+		while ($data=$req->fetch()) 
+		{
+			echo '<tr>';
+			echo '<td>'.$data["lesson_name"].'</td>';
+			echo '<td>'.get_user($data["user_id"]).'</td>';
+			echo '<td>'.get_group($data["group_id"]).'</td>';
+			echo '<td>'.get_lessons_ue($data["teachingunit_id"]).'</td>';
+			echo '<td >'.$data["lesson_coefficient"].'</td>';
+			echo '</tr>';
+		}
+	}
 	function get_lessons_list()
 	{
 		include("pdo.php");
--- lessons.php
+++ lessons.php
@@ -2,10 +2,10 @@
 	
 	session_start();
 
-	if ($_SESSION["rank"]==1 || $_SESSION["rank"]==2) 
+	if ($_SESSION["rank"]==2) 
 	{ ?>
 
-
+<!-- ADMINISTRATEUR -->
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -85,6 +85,80 @@
 <?php
 }
 
+else if ($_SESSION["rank"] == 1)
+{
+?>
+<!-- PROFESSEUR -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vue des cours</title>
+
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/local.css" />
+	<link rel="stylesheet" type="text/css" href="css/dataTables-utilisateur.css" />
+
+    <!--<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>-->
+	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>  
+	
+	<!-- you need to include : pour travailler sur les tableaux -->
+	<script type="text/javascript" src="js/dataTables-utilisateur.min.js"></script>  
+
+</head>
+<body>
+
+    <div id="wrapper">
+        <?php include("nav.php"); include("get_lessons.php");?>
+
+		<div class="row text-center">
+			<h2>Cours</h2>
+		</div>
+		<div class="row center-block">
+            <div class="col-lg-12 ">
+				<table id="example" class="display" cellspacing="0" width="100%">
+					<thead>
+						<tr>	
+							<th>Intitulé</th>
+							<th>Professeur</th>
+							<th>Groupe</th>
+							<th>Unité d'enseignement</th>
+							<th>Coefficient</th>
+						</tr>
+					</thead>
+			 
+					<tfoot>
+						<tr>
+							<th>Intitulé</th>
+							<th>Professeur</th>
+							<th>Groupe</th>
+							<th>Unité d'enseignement</th>
+							<th>Coefficient</th>
+						</tr>
+					</tfoot>
+			 
+					<tbody>
+						<?php get_prof_lessons($_SESSION["id"]); ?>
+					</tbody>
+				</table>
+            </div>
+        </div>  
+    </div>
+	<script type="text/javascript">
+	<!-- you need to include : pour travailler sur les tableaux -->
+        jQuery(function ($) {
+		$(document).ready(function() {
+			$('#example').dataTable( {"createdRow": function ( row, data, index ) {}} );} );
+		});
+    </script>
+
+</body>
+</html>
+<?php
+}
 else
 {
 	header("Location: index.php");
